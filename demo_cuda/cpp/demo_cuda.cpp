0b35d32e0b276ff4a3044bb294f30661f6203a48
zhangjiaxinghust <u201617119@hust.edu.cn>
2020-01-09 15:35:39 +0800

    add the time in c++

--- demo_cuda/cpp/demo_cuda.cpp
+++ demo_cuda/cpp/demo_cuda.cpp
@@ -3,6 +3,7 @@
 //
 #include "demo_cuda.h"
 #include "calculate.h"
+#include <time.h>
 
 namespace zjx {
 
@@ -19,6 +20,11 @@ demo_cuda::demo_cuda(int * pointer, int length) {
 demo_cuda::~demo_cuda () {}
 
 void demo_cuda::calculate(){
-  add_ceshi(this->pointer, this->length);
+  clock_t start_time=clock();
+    add_ceshi(this->pointer, this->length);
+  clock_t end_time=clock();
+  std::cout<<"time---------------------------------------"<<std::endl;
+  std::cout<< "Running time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;
+  std::cout<<"time---------------------------------------"<<std::endl;
 }
 }
--- demo_cuda/test.py
+++ demo_cuda/test.py
@@ -1,9 +1,15 @@
 import cuda_demo as demo_cuda
 import numpy as np
+import timeit 
+import time
 
-arry=range(1,55)
+arry=range(1,200000000)
 I=np.asarray(arry)
 pydemo=demo_cuda.Pydemo_cuda(I,len(I))
 print(I)
+since =time.time()
 pydemo.calculate()
-print(I)
+print(len(I))
+time_elapsed = time.time() - since
+print('Training complete in {:.0f}ms'.format(
+    time_elapsed*1000 %60000))
